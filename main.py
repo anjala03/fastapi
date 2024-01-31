524896ed326528f212740f282c09a11f177dc1a8
anjala03 <bhattaanjala13@gmail.com>
2024-01-31 15:42:07 +0545

    html to create the notes

--- main.py
+++ main.py
@@ -5,16 +5,28 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pymongo import MongoClient
 app=FastAPI()
-#app.mount("/static", StaticFiles(directory="static"), name="static")
-#templates = Jinja2Templates(directory="templates")
-#client= MongoClient("")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+# sets the connection with the database
+connection= MongoClient("mongodb+srv://anjalamongodb:mongodb@cluster0.b3vxizb.mongodb.net/Notes")
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-#@app.get("/", response_class=HTMLResponse)
-#async def read_items(request: Request):
-#    return templates.TemplateResponse("index.html", {"request":request})
+#@app.get("/")
+#def read_root():
+#    return {"Hello": "World"}
+@app.get("/", response_class=HTMLResponse)
+async def read_items(request: Request):
+    
+    docs= connection.Notes.notes.find({})
+    newdocs=[]
+
+    for doc in docs:
+       newdocs.append({
+           "id":doc["_id"],
+           "note":doc["note"]
+
+       })
+       print(doc)
+    return templates.TemplateResponse("index.html", {"request": request, "newdocs":newdocs})
 
 
 @app.get("/items/{item_id}")
